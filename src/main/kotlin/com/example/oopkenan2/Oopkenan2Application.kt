6c2cd6ca8927d08fb64226bca0c6fcc45357192b
abkenannx <kabdulkerimov@std.beu.edu.az>
2024-10-25 14:48:05 +0400

    1st

---
+++ src/main/kotlin/com/example/oopkenan2/Oopkenan2Application.kt
@@ -0,0 +1,11 @@
+package com.example.oopkenan2
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Oopkenan2Application
+
+fun main(args: Array<String>) {
+    runApplication<Oopkenan2Application>(*args)
+}
---
+++ src/test/kotlin/com/example/oopkenan2/Oopkenan2ApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.oopkenan2
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Oopkenan2ApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
